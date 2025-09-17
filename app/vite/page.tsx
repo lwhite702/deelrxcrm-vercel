98a44889ee521b7fb65d8784852da225bc9d1e73
Lee <lee@wrelik.com>
2025-09-17 03:21:09 +0000

    chore(migrate): embed Vite React app at /vite for incremental migration

---
+++ app/vite/page.tsx
@@ -0,0 +1,13 @@
+"use client"
+
+import React from 'react'
+import App from '../../DeelrzCRM/client/src/App'
+import '../../DeelrzCRM/client/src/index.css'
+
+export default function ViteMount() {
+  return (
+    <div id="vite-root" style={{ height: '100%' }}>
+      <App />
+    </div>
+  )
+}
