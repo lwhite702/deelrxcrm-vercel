76a010c5d3db3bf60ec47fb05711daa253903673
Lee <lee@wrelik.com>
2025-09-18 01:00:33 +0000

    chore(next): consolidate legacy index.css import into layout

--- app/customers/CustomersClient.tsx
+++ app/customers/CustomersClient.tsx
@@ -1,6 +1,5 @@
 "use client";
 import React from "react";
-import "../../DeelrzCRM/client/src/index.css";
 
 export default function CustomersClient() {
   return (
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import "./globals.css";
+import "../DeelrzCRM/client/src/index.css";
 import ClerkClientWrapper from "./components/ClerkClientWrapper";
 import ViteHeader from "./components/ViteHeader";
 import { Providers } from "./providers";
